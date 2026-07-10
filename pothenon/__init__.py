200b2d0e30a3c4cbaa96b4a165c75fde6e1bc954
Sam Waseda <waseda@mpie.de>
2026-07-10 16:43:55 +0200

    Update api and init

--- pothenon/__init__.py
+++ pothenon/__init__.py
@@ -1,8 +1,12 @@
 import importlib.metadata
 
+from pothenon.api import get_full_source
+
 try:
     # Installed package will find its version
     __version__ = importlib.metadata.version(__name__)
 except importlib.metadata.PackageNotFoundError:
     # Repository clones will register an unknown version
     __version__ = "0.0.0+unknown"
+
+__all__ = ["get_full_source"]
